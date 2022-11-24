baf4ac411fc3d69e660016a5733dedd7c6da7c8e
BDanielo <daniel@armieux.fr>
2022-11-24 11:32:38 +0100

    tout beau et fonctionnel sauf lien entre les pages

--- ProjetIOS/ContentView.swift
+++ ProjetIOS/ContentView.swift
@@ -45,11 +45,11 @@ struct ContentView: View {
                     Image(systemName: "square.split.2x2")
                     Text("Categories")
             }
-//            VueArticle()
-//                .tabItem {
-//                    Image(systemName: "cube.box.fill")
-//                    Text("Articles")
-//            }
+            VueArticles()
+                .tabItem {
+                    Image(systemName: "cube.box.fill")
+                    Text("Articles")
+            }
         }.myTabViewStyle()
     }
 }
@@ -188,6 +188,90 @@ struct VueCategories: View {
         for offset in offsets {
             let categorieChoisie = Requete[offset]
             Element.delete(categorieChoisie)
+        }
+        try? Element.save()
+    }
+}
+
+struct VueArticles: View {
+    
+    @FetchRequest(sortDescriptors: []) var Requete: FetchedResults<Articles>
+    @Environment(\.managedObjectContext) var Element
+    
+    @State var nomArticleChoisi: String = ""
+    @State var descArticleChoisi: String = ""
+    @State var qteArticleChoisi: String = ""
+    
+    @State var statutErreur: Bool = false
+    @State var messageErreur: String = ""
+    
+    @State var afficherNouvelArticle = false
+    
+    var body: some View {
+        VStack{
+            HStack{
+                Text("Liste des articles")
+                Button("+"){
+                    afficherNouvelArticle.toggle()
+                }
+            }
+            if afficherNouvelArticle {
+                VStack{
+                    TextField("Nom de l'article", text:$nomArticleChoisi).multilineTextAlignment(.center)
+                    TextField("Description de l'article", text:$descArticleChoisi).multilineTextAlignment(.center)
+                    TextField("Quantite de l'article", text:$qteArticleChoisi).multilineTextAlignment(.center)
+                    Button("Ajouter l'article") {
+                        ajoutArticle()
+                    }
+                }
+            }
+            List {
+                HStack {
+                    Text("Nom")
+                    Text("Qte")
+                }
+                ForEach(Requete) { Articles in
+                    HStack {
+                        Text(Articles.nom ?? "")
+                        Text(String(Articles.qte))
+                        Button("Plus d'infos") {
+                            //! lien a faire avec article pour la modif et afficher les infos
+                        }
+                    }
+                }.onDelete(perform: supprimerArticle)
+            }
+            
+            
+        }.alert(isPresented: $statutErreur) {
+            Alert(
+                title: Text("Erreur"),
+                message: Text(messageErreur)
+            )
+        }
+    }
+    
+    func ajoutArticle() {
+        let nomChoisi = nomArticleChoisi
+        let descChoisi = descArticleChoisi
+        if (!nomChoisi.isEmpty) {
+            let qteChoisi : Int16 = Int16(qteArticleChoisi) ?? 0
+            let article = Articles(context: Element)
+            article.id = UUID()
+            article.nom = "\(nomChoisi)"
+            article.desc = "\(descChoisi)"
+            article.qte = qteChoisi
+            try? Element.save()
+        }
+        else {
+            statutErreur=true
+            messageErreur="Le nom de l'article ne doit pas etre vide"
+        }
+    }
+    
+    func supprimerArticle(at offsets: IndexSet) {
+        for offset in offsets {
+            let articleChoisi = Requete[offset]
+            Element.delete(articleChoisi)
         }
         try? Element.save()
     }
